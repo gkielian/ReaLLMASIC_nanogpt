4b40a917dd92ad7048845ed647a47aee722fa23c
Gregory Kielian <gkielian@google.com>
2023-11-01 14:32:10 -0700

    Add help menu and parameterization to data script

--- data/modular_addition/create_examples.sh
+++ data/modular_addition/create_examples.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-declare -a bases=("1" "2" "4" "8" "16")
+print_help(){
+  echo "Usage: $0 [options]
+  Print numbers in different bases to files.
 
+  Options:
+  -h, --help      Show this help message and exit
+  -m, --modulo    Modulo number (default: 128)
+"
+}
+
+declare -a bases=("1" "2" "4" "8" "16")
 data_dir="data"
+modulo=128
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h|--help)
+      print_help
+      exit 0
+      ;;
+    -m|--modulo)
+      modulo="$2"
+      shift
+      ;;
+    *)
+      echo "Unknown option $1"
+      print_help
+      exit 1
+      ;;
+  esac
+  shift
+done
 
 if [ ! -d "${data_dir}" ]; then
   mkdir -p "${data_dir}"
@@ -10,8 +39,5 @@ fi
 
 for i in "${bases[@]}"; do
   echo "$i"
-  python print_bases_mod_x.py --modulo 16 --no_separator --base "$i" --seed 16 > "./${data_dir}/base_${i}.txt"
-  # python print_bases_mod_x.py --modulo 128  --base "$i" --seed 16 > "./${data_dir}/base_${i}.txt"
+  python print_bases_mod_x.py --modulo "$modulo" --no_separator --base "$i" --seed 16 > "./${data_dir}/base_${i}.txt"
 done
-
-
