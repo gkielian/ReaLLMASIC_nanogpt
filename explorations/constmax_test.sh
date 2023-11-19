e7798cade89f0f72f29bb6c0dfdd66cabf898f8d
Gregory Kielian <gkielian@google.com>
2023-11-18 21:23:01 -0800

    Change dataset back to shakespeare
    Seems that we're overloading the A100 80GB type GPU with the openwebtext
    settings, definitely want to get the openwebtext size dataset eventually
    but for now will change back to shakespeare with tiktoken for testing.

--- explorations/constmax_test.sh
+++ explorations/constmax_test.sh
@@ -3,25 +3,32 @@
 # head to repo root
 cd ../
 
-for seed in {1337..1387..2}; do
+pushd ../data/shakespeare/
+
+python3 prepare.py
+
+popd
+
+for seed in {1337..1347..2}; do
+  # TODO: find larger model that aggrees with A100, maybe try:
+  # --n_layer 12 \
+  # --n_head 12 \
+  # --n_embd 768 \
 
   python3 train.py \
     --max_iters 8000 \
     --eval_iters 200 \
     --eval_interval 100 \
     --log_interval 10 \
-    --n_layer 24 \
-    --n_head 16 \
-    --n_embd 1024 \
     --seed_offset "$seed" \
-    --dataset "openwebtext" \
+    --dataset "shakespeare" \
     --use_post_ln \
     --use_softmax_variant \
     --softmax_variant "constantmax" \
-    --tensorboard_project "owt_tiktoken_temperature" \
+    --tensorboard_project "shakespeare_tiktoken" \
     --tensorboard_run_name "constmax_temperature_${seed}" \
-    --block_size 2048 \
-    --out_dir "constantmax_openwebtext_temp" \
+    --block_size 1024 \
+    --out_dir "constantmax_ts_temp" \
     --compile
 
 done
