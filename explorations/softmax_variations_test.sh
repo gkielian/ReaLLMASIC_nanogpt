0956214449e3e4ed2f922d3b53b84b264671d17a
Gregory Kielian <gkielian@google.com>
2023-11-20 18:20:40 -0800

    Further parameterize softmax exploration script

--- explorations/softmax_variations_test.sh
+++ explorations/softmax_variations_test.sh
@@ -3,40 +3,54 @@
 # head to repo root
 cd ../
 
-softmax_variation=("softermax" "polymax" "constantmax")
+dataset="shakespeare_char"
+python3 "data/${dataset}/prepare.py"
 
-# do one iteration with regular softmax
-softmax_variant="regular_softmax"
-python3 train.py \
-  --max_iters 16000 \
-  --eval_iters 200 \
-  --eval_interval 200 \
-  --log_interval 10 \
-  --dataset "tinystories_en" \
-  --use_post_ln \
-  --no-use_softmax_variant \
-  --tensorboard_project "ts_en_${softmax_variant}_explorations_post_ln" \
-  --tensorboard_run_name "${softmax_variant}" \
-  --block_size 1024 \
-  --out_dir "ts_en_${softmax_variant}_evaluations_post_ln" \
-  --compile
+softmax_variation=("constantmax")
+
+# TODO: uncomment before submitting final constantmax PR
+# softmax_variation=("constantmax" "polymax" "softermax" "constantmax" "sigsoftmax")
+
+max_iters="8000"
+block_size="1024"
+notes="add_custom_notes_here"
 
 # Loop over the array
 for softmax_variant in "${softmax_variation[@]}"
 do
-  softmax_variant="$element"
-
   python3 train.py \
-    --max_iters 16000 \
+    --max_iters "$max_iters" \
     --eval_iters 200 \
     --eval_interval 200 \
     --log_interval 10 \
-    --dataset "tinystories_en" \
+    --device cuda \
+    --dataset "$dataset" \
     --use_post_ln \
-    --no-use_softmax_variant \
-    --tensorboard_project "ts_en_${softmax_variant}_explorations_post_ln" \
-    --tensorboard_run_name "${softmax_variant}" \
-    --block_size 1024 \
-    --out_dir "ts_en_${softmax_variant}_evaluations_post_ln" \
+    --use_softmax_variant \
+    --softmax_variant "${softmax_variant}" \
+    --use_softermax_xmax \
+    --tensorboard_project "${dataset}_${softmax_variant}_${max_iters}" \
+    --tensorboard_run_name "${softmax_variant}_${notes}" \
+    --block_size "$block_size" \
+    --out_dir "${dataset}_${softmax_variant}_${max_iters}_${notes}" \
     --compile
 done
+
+# do one iteration with regular softmax
+softmax_variant="regular_softmax"
+python3 train.py \
+  --max_iters "$max_iters" \
+  --eval_iters 200 \
+  --eval_interval 200 \
+  --log_interval 10 \
+  --device cuda \
+  --dataset "$dataset" \
+  --use_post_ln \
+  --no-use_softmax_variant \
+  --use_softermax_xmax \
+  --tensorboard_project "${dataset}_${softmax_variant}_${max_iters}" \
+  --tensorboard_run_name "${softmax_variant}_${notes}" \
+  --block_size "$block_size" \
+  --out_dir "${dataset}_${softmax_variant}_${max_iters}_${notes}" \
+  --compile
+
