eeee1bfbfcf5301c87af5ce7a340b8523d252eb3
Gregory Kielian <gkielian@google.com>
2025-06-05 00:44:54 +0000

    Spelling fix

--- demos/dataset_colorization.sh
+++ demos/dataset_colorization.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 # demos/dataset_colorization.sh
 
-pushd data/filipino/tagalog_filipino_eng_translation
-bash get_dataset.sh
-popd
+# pushd data/filipino/tagalog_filipino_eng_translation
+# bash get_dataset.sh
+# popd
 
-python train.py \
-  --dataset filipino/tagalog_filipino_eng_translation \
-  --compile \
-  --max_iters 10000
+# python train.py \
+  # --dataset filipino/tagalog_filipino_eng_translation \
+  # --compile \
+  # --max_iters 10000
 
 python colorize_dataset.py \
-  --out_dir        out \
+  --out_dir        out/inits-True-False-2500-2500-iteration-12-12-768-1024-True-True-1.0-512-True-all-cuda-bfloat16-filipino/tagalog_filipino_eng_translation-True \
   --dataset        filipino/tagalog_filipino_eng_translation  \
   --split          val \
   --num_tokens     2048 \
   --device         cuda:0 \
-  --block_size     256 \
-  --mode           minmax \
-  --output_file    kulay_na_dataset.txt
+  --block_size     1024 \
+  --mode           minmax  \
+  --output_file    kulay_ng_dataset_minmax.txt
+
+python colorize_dataset.py \
+  --out_dir        out/inits-True-False-2500-2500-iteration-12-12-768-1024-True-True-1.0-512-True-all-cuda-bfloat16-filipino/tagalog_filipino_eng_translation-True \
+  --dataset        filipino/tagalog_filipino_eng_translation  \
+  --split          val \
+  --num_tokens     2048 \
+  --device         cuda:0 \
+  --block_size     1024 \
+  --mode           softmax  \
+  --output_file    kulay_ng_dataset_softmax.txt
